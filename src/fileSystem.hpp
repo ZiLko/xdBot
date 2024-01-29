40b075dbd45719730dfd6e86ee6a1a425f581025
ZIlko <69888540+ZiLko@users.noreply.github.com>
2024-01-28 22:46:57 -0600

    Create fileSystem.hpp

---
+++ src/fileSystem.hpp
@@ -0,0 +1,309 @@
+#include <Geode/Geode.hpp>
+#define CCPOINT_CREATE(__X__,__Y__) cocos2d::CCPointMake((float)(__X__), (float)(__Y__))
+
+using namespace geode::prelude;
+class macroCell : public CCNode {
+public:
+    static macroCell* create(std::string name) {
+        auto ret = new macroCell();
+        if (ret && ret->init(name)) {
+            ret->autorelease();
+        } else {
+            delete ret;
+            ret = nullptr;
+        }
+    return ret;
+    }
+
+    void loadMacro(CCObject*);
+    void handleLoad(CCObject*);
+
+    void handleDelete(CCObject* btn) {
+	    std::string path = Mod::get()->getSaveDir().string()
+        +"\\"+static_cast<CCMenuItemSpriteExtra*>(btn)->getID() + ".xd";
+        if (std::remove(path.c_str()) != 0) {
+            FLAlertLayer::create(
+    		"Delete Macro",   
+    		"There was an <cr>error</c> deleting this macro.",  
+    		"OK"      
+			)->show();
+        } else {
+            if (this->getParent()->getParent()->getChildren()->count() == 1)  {
+                CCLabelBMFont* noMacroLbl = CCLabelBMFont::create("No macros.", "bigFont.fnt");
+                noMacroLbl->setAlignment(CCTextAlignment::kCCTextAlignmentCenter);
+                noMacroLbl->setColor(ccc3(201,170,153));
+                noMacroLbl->setScale(0.725f);
+                this->getParent()->getParent()->getParent()->addChild(noMacroLbl);
+                noMacroLbl->setPosition(this->getParent()->getParent()->getParent()->getContentSize() / 2);
+            }
+            this->getParent()->removeFromParentAndCleanup(true);
+            FLAlertLayer::create(
+    		"Delete Macro",   
+    		"Macro deleted <cg>successfully</c>.",  
+    		"OK"      
+			)->show();
+        }
+    }
+
+    void deleteMacro(CCObject* btn) {
+        geode::createQuickPopup(
+    		"Load Macro",     
+    		"<cr>Delete</c> this macro?", 
+    		"Cancel", "Ok",  
+    		[this, btn](auto, bool btn2) {
+        		if (btn2) this->handleDelete(btn);
+    		}); 
+    }
+
+    bool init(std::string name) {
+        auto lbl = (name.length() > 19) ? (name.substr(0, 19) + "...") : name;
+        CCLabelBMFont* macroLabel = CCLabelBMFont::create(lbl.c_str(), "bigFont.fnt");
+        macroLabel->setAnchorPoint(ccp(0.0f,0.5f));
+        macroLabel->setAlignment(CCTextAlignment::kCCTextAlignmentLeft);
+        macroLabel->setPosition({5, 20});
+        macroLabel->setScale(0.45f);
+        this->addChild(macroLabel);
+        auto menu = CCMenu::create();
+        menu->setPosition({0,0});
+
+        auto btnSpr1 = ButtonSprite::create("Load");
+        btnSpr1->setScale(0.7f);
+        auto btn = CCMenuItemSpriteExtra::create(btnSpr1,
+   		this,
+   		menu_selector(macroCell::loadMacro));
+        btn->setPosition({270,20});
+        menu->addChild(btn);
+        btn->setID(name);
+        
+        auto btnSpr = CCSprite::createWithSpriteFrameName("GJ_trashBtn_001.png");
+        btnSpr->setScale(0.65f);
+        btn = CCMenuItemSpriteExtra::create(
+        btnSpr,
+        this,
+        menu_selector(macroCell::deleteMacro)
+        );
+        btn->setPosition({225,20});
+        btn->setID(name);
+        menu->addChild(btn);
+        this->addChild(menu);
+        return true;
+    }
+};
+
+class loadMacroPopup : public geode::Popup<std::string const&> {
+public:
+    void importMacro(CCObject*) {
+        file::FilePickOptions fileOptions;
+        file::FilePickOptions::Filter textFilter;
+        textFilter.description = "Text Files";
+        textFilter.files = {"*.xd"};
+        fileOptions.filters.push_back(textFilter);
+
+        file::pickFile(file::PickMode::OpenFile , fileOptions, [this](ghc::filesystem::path result) {
+            auto path = ghc::filesystem::path(result.c_str());
+            std::ifstream sourceMacro(path);
+            if (!sourceMacro.is_open()) {
+            FLAlertLayer::create(
+    		    "Import Macro",   
+    		    "An <cr>error<c/> occurred while importing this macro.",  
+    		    "OK"      
+			)->show();
+            return;
+            }
+            std::ofstream copiedMacro(Mod::get()->getSaveDir().string()
+            + "\\" + path.filename().string(), std::ios::binary);
+
+            if (!copiedMacro.is_open()) {
+                FLAlertLayer::create(
+    		        "Import Macro",   
+    		        "An <cr>error<c/> occurred while importing this macro.",  
+    		        "OK"      
+			    )->show();
+                return;
+            }
+
+            copiedMacro << sourceMacro.rdbuf();
+
+            sourceMacro.close();
+            copiedMacro.close();
+            refresh();
+            FLAlertLayer::create(
+    		    "Import Macro",   
+    		    "Macro imported <cg>successfully</c>.",  
+    		    "OK"      
+			)->show();
+        });
+    }
+
+    bool setup(std::string const& value) override {
+        CCArray* macroList = CCArray::create();
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        Result<std::vector<ghc::filesystem::path>> macros;
+        std::string titleLol;
+        std::string noBUMessage;
+        CCPoint corner = winSize/2.f-CCPOINT_CREATE(m_size.width/2.f,m_size.height/2.f);
+        macros = file::readDirectory(Mod::get()->getSaveDir());
+
+        auto emptyBtn = CCSprite::createWithSpriteFrameName("GJ_plainBtn_001.png");
+        emptyBtn->setScale(0.75f);
+        auto folderIcon = CCSprite::createWithSpriteFrameName("folderIcon_001.png");
+        folderIcon->setPosition(emptyBtn->getContentSize() / 2);
+        folderIcon->setScale(0.8f);
+        emptyBtn->addChild(folderIcon);
+        auto menu = CCMenu::create();
+        menu->setPosition({0,0});
+
+        CCSprite* importMacroSprite = CCSprite::createWithSpriteFrameName("GJ_plusBtn_001.png");
+        importMacroSprite->setScale(0.8f);
+        auto importMacroButton = CCMenuItemSpriteExtra::create(
+            importMacroSprite,
+            this,
+            menu_selector(loadMacroPopup::importMacro)
+        );
+        importMacroButton->setPosition(corner + CCPOINT_CREATE(380,65));
+        menu->addChild(importMacroButton);
+
+        m_mainLayer->addChild(menu);
+        auto openFolderBtn = CCMenuItemSpriteExtra::create(
+            emptyBtn,
+            this,
+            menu_selector(loadMacroPopup::openMacrosFolder)
+        );
+        openFolderBtn->setPosition(corner + CCPOINT_CREATE(380,20));
+        menu->addChild(openFolderBtn);
+
+        for (int i = macros.value().size(); i--> 0;) {
+            std::ifstream file;
+            if (macros.value()[i].extension() == ".xd") {
+                file.open(macros.value()[i].string());
+                if (file){
+                    macroCell* cell = macroCell::create(macros.value()[i].filename().string().substr(0, macros.value()[i].filename().string().find_last_of('.')));
+                    macroList->addObject(cell);
+                }
+            }
+        }
+
+        ListView* mcrList = ListView::create(macroList, 40, 308, 200);
+
+        mcrList->setScaleY(0.875f);
+        mcrList->setScaleX(0.89f);
+        mcrList->setPosition({-14, -9.5f});
+
+        auto list = GJListLayer::create(mcrList, "Load Macro", {0, 0, 0, 0}, 280, 180, 2);
+
+        m_mainLayer->addChild(list);
+
+        CCArray* listChildren = list->getChildren();
+
+        static_cast<CCSprite*>(listChildren->objectAtIndex(0))->setScale(0.8f);
+        CCSprite* topS = static_cast<CCSprite*>(listChildren->objectAtIndex(1));
+        topS->setScale(0.8f);
+        topS->setPosition({140, 191});
+
+        CCLabelBMFont* label = static_cast<CCLabelBMFont*>(listChildren->objectAtIndex(4));
+        label->setScale(0.7f);
+        label->setPosition({140, 194});
+        list->setPosition(corner + CCPOINT_CREATE(38.8,35));
+
+        if (macroList->count() == 0) {
+            CCLabelBMFont* noMacroLbl = CCLabelBMFont::create("No macros.", "bigFont.fnt");
+            noMacroLbl->setAlignment(CCTextAlignment::kCCTextAlignmentCenter);
+            noMacroLbl->setColor(ccc3(201,170,153));
+            noMacroLbl->setScale(0.725f);
+
+            list->addChild(noMacroLbl);
+            noMacroLbl->setPosition(list->getContentSize() / 2);
+        }
+        return true;
+    }
+
+    static loadMacroPopup* create() {
+        auto ret = new loadMacroPopup();
+        if (ret && ret->init(355, 255, "")) {
+            ret->autorelease();
+            return ret;
+        }
+        CC_SAFE_DELETE(ret);
+        return nullptr;
+    }
+
+	void openLoadMenu(CCObject*) {
+		create()->show();
+	}
+
+    void refresh() {
+        this->keyBackClicked();
+        CCObject* fill;
+        openLoadMenu(fill);
+    }
+
+    void openMacrosFolder(CCObject*) {
+        file::openFolder(Mod::get()->getSaveDir());
+    }
+};
+
+
+class saveMacroPopup : public geode::Popup<std::string const&> {
+public:
+    CCTextInputNode* macroNameInput = nullptr;
+
+    bool setup(std::string const& value) override {
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+        auto inputBg = CCScale9Sprite::create("square02b_001.png", {0, 0, 80, 80});
+
+        inputBg->setScale(0.7f);
+        inputBg->setColor({ 0, 0, 0 });
+        inputBg->setOpacity(75);
+        inputBg->setPosition(winSize/2 + CCPOINT_CREATE(0,7));
+        inputBg->setContentSize({ 235, 54 });
+
+        macroNameInput = CCTextInputNode::create(150, 30, "Macro Name", "bigFont.fnt");
+        macroNameInput->m_textField->setAnchorPoint({ 0.5f, 0.5f });
+        macroNameInput->ignoreAnchorPointForPosition(true);
+        macroNameInput->m_placeholderLabel->setAnchorPoint({ 0.5f, 0.5f });
+        macroNameInput->setPosition(winSize/2 + CCPOINT_CREATE(0,7));
+        macroNameInput->setMaxLabelScale(0.7f);
+        macroNameInput->setLabelPlaceholderColor(ccc3(163, 135, 121));
+        macroNameInput->setMouseEnabled(true);
+        macroNameInput->setTouchEnabled(true);
+
+        auto title = CCLabelBMFont::create("Save Macro", "bigFont.fnt");
+        title->setPosition(winSize/2 + CCPOINT_CREATE(0,50));
+        title->setScale(0.6f);
+        m_mainLayer->addChild(title);
+
+        auto btnSpr = ButtonSprite::create("Save");
+        btnSpr->setScale(0.9f);
+        auto menu = CCMenu::create();
+        menu->setPosition({0,0});
+        auto btn = CCMenuItemSpriteExtra::create(
+            btnSpr,
+            this,
+            menu_selector(saveMacroPopup::saveMacro)
+        );
+
+        btn->setPosition(winSize/2 + CCPOINT_CREATE(0,-40));
+        menu->addChild(btn);
+        m_mainLayer->addChild(menu);
+
+        m_mainLayer->addChild(inputBg);
+        m_mainLayer->addChild(macroNameInput);
+        return true;
+    }
+
+    static saveMacroPopup* create() {
+        auto ret = new saveMacroPopup();
+        if (ret && ret->init(220, 140, "")) {
+            ret->autorelease();
+            return ret;
+        }
+        CC_SAFE_DELETE(ret);
+        return nullptr;
+    }
+
+	void openSaveMacro(CCObject*);
+
+    void saveMacro(CCObject*);
+   
+};
